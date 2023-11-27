f16a8e08673ae167c395b54cf59854521fcbd64e
lumpn <lumpn@users.noreply.github.com>
2023-11-27 19:04:14 +0800

    Removed debug messages

--- WebGL/PersistenceUtils.cs
+++ WebGL/PersistenceUtils.cs
@@ -13,30 +13,21 @@ namespace Lumpn.Persistence
         public static Task SaveAsync<T>(string fileName, T data)
         {
             var filePath = GetFilePath(fileName);
-
-            Debug.LogFormat("writing data to {0}", filePath);
             var contents = JsonUtility.ToJson(data, false);
-            Debug.LogFormat("playerprefs set string {0}: {1}", filePath, contents);
             PlayerPrefs.SetString(filePath, contents);
-            Debug.LogFormat("done");
             return Task.CompletedTask;
         }
 
         public static Task<T> LoadAsync<T>(string fileName)
         {
             var filePath = GetFilePath(fileName);
-
-            Debug.LogFormat("reading data from {0}", filePath);
             var contents = PlayerPrefs.GetString(filePath);
-            Debug.LogFormat("playerprefs get string {0}: {1}", filePath, contents);
             if (string.IsNullOrEmpty(contents))
             {
-                Debug.LogFormat("none");
                 return Task.FromResult<T>(default);
             }
 
             var data = JsonUtility.FromJson<T>(contents);
-            Debug.LogFormat("done");
             return Task.FromResult<T>(data);
         }
 
